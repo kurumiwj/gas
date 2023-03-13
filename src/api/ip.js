67a900155b010e846e999a89e5161a725c21317c
kurumiwj <44957992+kurumiwj@users.noreply.github.com>
2023-03-13 23:20:59 +0800

    Delete src directory

--- src/api/ip.js
+++
@@ -1,2 +0,0 @@
-const ip="http://127.0.0.1:8082";
-export default ip;
--- src/utils/commonPosition.js
+++
@@ -1,6 +0,0 @@
-function findCommonPosition(str){
-    let pos=str.lastIndexOf("-");
-    return str.slice(0,pos);
-}
-
-export {findCommonPosition};
